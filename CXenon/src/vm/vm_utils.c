6545ca4c9cfba6963163547e8d207fb5c98e0f4e
ikbenlike <thewatcheriswatchinu@gmail.com>
2017-07-12 20:53:31 +0200

    implements vm_free() and enables xenon_print_stack_item() to print functions

--- CXenon/src/vm/vm_utils.c
+++ CXenon/src/vm/vm_utils.c
@@ -16,7 +16,7 @@
     NULL on error; `xenon_vm_t*` on succes.
 */
 
-xenon_vm_t *xenon_vm_generate(int startip, int stacksize, int codesize){
+xenon_vm_t *xenon_vm_generate(int startip, int stacksize, int codesize, int cstacksize){
     xenon_vm_t *vm = calloc(1, sizeof(xenon_vm_t));
     if(vm == NULL){
         fprintf(stderr, "%s\n", strerror(errno));
@@ -37,6 +37,10 @@ xenon_vm_t *xenon_vm_generate(int startip, int stacksize, int codesize){
         return NULL;
     }
     vm->code.len = codesize;
+    if(cstacksize == 0){
+        cstacksize = stacksize * 0.5;
+    }
+    vm->ctx = calloc(cstacksize, sizeof(xenon_context_t));
     return vm;
 }
 
@@ -70,8 +74,34 @@ xenon_stack_item_t xenon_string_generate(char *value){
     return i;
 }
 
-inline void xenon_stack_append(xenon_stack_t stack, xenon_stack_item_t item){
-    stack.stack[stack.cursor++] = item;
+xenon_stack_item_t xenon_bool_generate(bool value){
+    xenon_stack_item_t i;
+    i.type = boolean;
+    i.boolean = value;
+    return i;
+}
+
+xenon_stack_item_t xenon_func_generate(int nargs, int nlocals, int addr){
+    xenon_stack_item_t i;
+    i.type = function;
+    i.func.nargs = nargs;
+    i.func.nlocals = nlocals;
+    i.func.addr = addr;
+    return i;
+}
+
+void xenon_stack_append(xenon_stack_t *stack, xenon_stack_item_t item){
+    stack->stack[stack->cursor++] = item;
+    if(((float)stack->cursor / (float)stack->len) * 100 > 80){
+        void *tmp = realloc(stack->stack, (int)(stack->len * 1.75));
+        if(tmp == NULL){
+            puts("Xenon: invalid stack resize");
+            return;
+        }
+        else {
+            stack->stack = tmp;
+        }
+    }
     return;
 }
 
@@ -81,4 +111,38 @@ size_t xenon_print_xstring(xenon_string_t str){
         putchar(str.contents[i]);
     }
     return i;
+}
+
+int xenon_print_stack_item(xenon_stack_item_t item){
+    if(item.type == integer){
+        printf("%d\n", item.integer);
+    }
+    else if(item.type == floating){
+        printf("%f\n", item.floating);
+    }
+    else if(item.type == boolean){
+        if(item.boolean){
+            puts("true");
+        }
+        else {
+            puts("false");
+        }
+    }
+    else if(item.type == character){
+        printf("%c\n", item.character);
+    }
+    else if(item.type == string){
+        xenon_print_xstring(item.string);
+    }
+    else if(item.type == function){
+        printf("function {\n\tnargs: %d\n\tnlocals: %d\n\taddr: %d\n}\n", item.func.nargs, item.func.nlocals, item.func.addr);
+    }
+    return 0;
+}
+
+void vm_free(xenon_vm_t *vm){
+    free(vm->stack.stack);
+    free(vm->code.stack);
+    free(vm->ctx);
+    free(vm);
 }
--- CXenon/src/vm/vm_utils.h
+++ CXenon/src/vm/vm_utils.h
@@ -1,12 +1,15 @@
 #ifndef __XENON_VM_UTILS_H_
     #define __XENON_VM_UTILS_H_
 
-    xenon_vm_t *xenon_vm_generate(int startip, int stacksize, int codesize);
+    xenon_vm_t *xenon_vm_generate(int startip, int stacksize, int codesize, int cstacksize);
     xenon_stack_item_t xenon_int_generate(int value);
     xenon_stack_item_t xenon_opcode_generate(enum xenon_opcodes opcode);
     xenon_stack_item_t xenon_float_generate(float value);
     xenon_stack_item_t xenon_string_generate(char *value);
-    void xenon_stack_append(xenon_stack_t stack, xenon_stack_item_t item);
+    xenon_stack_item_t xenon_func_generate(int nargs, int nlocals, int addr);
+    void xenon_stack_append(xenon_stack_t *stack, xenon_stack_item_t item);
     size_t xenon_print_xstring(xenon_string_t str);
+    int xenon_print_stack_item(xenon_stack_item_t item);
+    void vm_free(xenon_vm_t *vm);
 
 #endif
