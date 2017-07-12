7d8f4e853136cb3d1bf1156187ba3ed5a548b2f3
ikbenlike <thewatcheriswatchinu@gmail.com>
2017-07-12 22:38:14 +0200

    adds xenon_bool_generate() to vm_utils.h

--- CXenon/src/vm/vm_utils.h
+++ CXenon/src/vm/vm_utils.h
@@ -6,6 +6,7 @@
     xenon_stack_item_t xenon_opcode_generate(enum xenon_opcodes opcode);
     xenon_stack_item_t xenon_float_generate(float value);
     xenon_stack_item_t xenon_string_generate(char *value);
+    xenon_stack_item_t xenon_bool_generate(bool value);
     xenon_stack_item_t xenon_func_generate(int nargs, int nlocals, int addr);
     void xenon_stack_append(xenon_stack_t *stack, xenon_stack_item_t item);
     size_t xenon_print_xstring(xenon_string_t str);
